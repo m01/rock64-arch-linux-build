09a78788d94c6e5d8f403cfdc3cc968f9f64c997
Michiel <code@m01.eu>
2018-03-12 19:19:10 +0000

    Imported latest Makefile.atf.mk and Makefile.uboot.mk from upstream ayufan repo https://github.com/ayufan-rock64/linux-build/

--- Makefile.atf.mk
+++ Makefile.atf.mk
@@ -1,12 +1,12 @@
-BL31 ?= arm-trusted-firmware/build/rk3328/release/bl31/bl31.elf
+BL31 ?= arm-trusted-firmware/build/$(ATF_PLAT)/release/bl31/bl31.elf
 
-arm-trusted-firmware/build/rk3328/release/bl31/bl31.elf: arm-trusted-firmware
+arm-trusted-firmware/build/$(ATF_PLAT)/release/bl31/bl31.elf: arm-trusted-firmware
 	make -C $< realclean
-	make -C $< CROSS_COMPILE=aarch64-linux-gnu- PLAT=rk3328 bl31
+	make -C $< CROSS_COMPILE=aarch64-linux-gnu- M0_CROSS_COMPILE=arm-linux-gnueabi- PLAT=$(ATF_PLAT) bl31
 
-arm-trusted-firmware/build/rk3328/debug/bl31/bl31.elf: arm-trusted-firmware
+arm-trusted-firmware/build/$(ATF_PLAT)/debug/bl31/bl31.elf: arm-trusted-firmware
 	make -C $< realclean
-	make -C $< CROSS_COMPILE=aarch64-linux-gnu- PLAT=rk3328 bl31 DEBUG=1
+	make -C $< CROSS_COMPILE=aarch64-linux-gnu- M0_CROSS_COMPILE=arm-linux-gnueabi- PLAT=$(ATF_PLAT) bl31 DEBUG=1
 
 .PHONY: atf-build
 atf-build: $(BL31)
