8e9e1b240c669b1b38667d4188404aab500b6ace
pico.dev <pico.dev@gmail.com>
2018-03-15 10:04:04 +0100

    Remove container once image is generated

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-docker stop rock64-arch-linux-build
-docker rm rock64-arch-linux-build
 docker run --name rock64-arch-linux-build rock64/rock64-arch-linux-build:latest bash -c "(cd /home/rock64/ && ./make.sh)"
 docker cp rock64-arch-linux-build:/home/rock64/rock64-arch-linux-build/arch.img arch.img
+docker rm rock64-arch-linux-build
