d85ed47155ae36f6ce13118d7a64c3425cecee5f
pico.dev <pico.dev@gmail.com>
2018-03-17 00:18:05 +0100

    Externalize repositories for allow personalize the build
    With a docker volume

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-docker run --name rock64-arch-linux-build rock64/rock64-arch-linux-build:latest bash -c "(cd /home/rock64/ && ./make.sh)"
-docker cp rock64-arch-linux-build:/home/rock64/rock64-arch-linux-build/arch.img arch.img
-docker rm rock64-arch-linux-build
+docker run --rm --name rock64-arch-linux-build --volume "$(dirname $(pwd))":/rock64-arch-linux-build rock64/rock64-arch-linux-build:latest
--- docker/make.sh
+++ docker/make.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-su - rock64 -c "git clone https://github.com/m01/rock64-arch-linux-build.git"
-su - rock64 -c "(cd rock64-arch-linux-build && make arch.img)"
+(cd /rock64-arch-linux-build && make arch.img)
